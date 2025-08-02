c47ac19981ff844a2cc9798839c7774da8417f9f
carolina torrens <torrenscarolina36@gmail.com>
2025-08-01 20:18:41 -0400

    add background

--- project.js
+++ project.js
@@ -1,38 +1,76 @@
-console.log("hola");
+console.log("hola")
+
+// let num = parseInt(prompt("Por favor ingresa un numero?","")) //para q se use hasta en internet explore
+
+// for (let i = 1; i <= num; i ++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log("FizzBuzz")
+//     }else if(i % 3 === 0){
+//         console.log("Fizz")
+//     }else if(i % 5 === 0){
+//         console.log("Buzz")
+//     }else{
+//         console.log(i)
+//     }
+// }
 
 
-// eleccion de la computadora 
 
-function getComputerChoice (){
-    let getComputer = Math.floor(Math.random()*3);
-    let election;
-    if (getComputer == 0){
-        election ="Scissors"
-        console.log(election);  
-    } else if (getComputer == 1){
-        election ="Rock"
-        console.log(election);
-    }else {
-        election ="Paper"
-        console.log(election);
-    }
+// do {}while(condicion) // la primera vez si se va a ejecutar el codigo q esta antes del bucle 
+
+// break //
+// continue // fuerza a q salte a la siguiente condicion incluso si no ha terminado el codigo 
+// estos 3 son modales el codigo se para hasta que el usuario hace algo
+alert("hola") // funciones integradas del navegador solo muestra un mensaje  
+prompt() //pedir datos
+let b = confirm("Esta seguro que quieres borrarlo?") //dos opciones. esto es un booleano 
+console.log(b)
+
+// console.log(typeof num)
+
+function hola(name){
+    alert("hola" + name);
 }
 
-getComputerChoice(); 
+hola(" caro")
 
 
-// eleccion humana 
 
-function getHumanChoice(){
-    let electionHuman = prompt("choose Rock Paper or Scissors");
-    console.log(electionHuman);
-}
-
-getHumanChoice();
+// console.log("hola");
 
 
-// variables globales 
+// // eleccion de la computadora 
 
-let humanScore = 0;
-let computerScore = 0; 
+// function getComputerChoice (){
+//     let getComputer = Math.floor(Math.random()*3);
+//     let election;
+//     if (getComputer == 0){
+//         election ="Scissors"
+//         console.log(election);  
+//     } else if (getComputer == 1){
+//         election ="Rock"
+//         console.log(election);
+//     }else {
+//         election ="Paper"
+//         console.log(election);
+//     }
+// }
+
+// getComputerChoice(); 
+
+
+// // eleccion humana 
+
+// function getHumanChoice(){
+//     let electionHuman = prompt("choose Rock Paper or Scissors");
+//     console.log(electionHuman);
+// }
+
+// getHumanChoice();
+
+
+// // variables globales 
+
+// let humanScore = 0;
+// let computerScore = 0; 
 
